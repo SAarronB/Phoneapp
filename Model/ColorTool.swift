d93fef56ca0d645f59310241b67763dd1258c4bd
SAaronB <programmingsean@gmail.com>
2018-10-09 08:44:38 -0600

    Added color file

---
+++ Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  PhoneApp
+//
+//  Created by Bonilla, Sean on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
